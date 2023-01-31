d6ad48b2d538c03953914a43b20804b77ae1078d
hl2703 <s10186258@connect.np.edu.sg>
2023-01-31 14:19:57 +0800

    Add api gateway

---
+++ BookAppAPIGateway/Program.cs
@@ -0,0 +1,20 @@
+using ikvm.runtime;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IWebHostBuilder builder = new WebHostBuilder();
+        builder.ConfigureServices(s =>
+        {
+            s.AddSingleton(builder);
+        });
+        builder.UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .UseStartup<Startup>()
+               .UseUrls("http://localhost:9000");
+
+        var host = builder.Build();
+        host.Run();
+    }
+}
