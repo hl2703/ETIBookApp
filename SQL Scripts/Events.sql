f78d6ad8226a334851efb00e6e7a505c6671e170
hl2703 <s10186258@connect.np.edu.sg>
2023-01-31 13:18:36 +0800

    Add sql scripts

---
+++ SQL Scripts/Events.sql
@@ -0,0 +1,20 @@
+Create Database myEventsDB
+
+CREATE TABLE myEvents
+(
+  EventID				int NOT NULL,
+  EventName				varchar(255) 	NOT NULL,
+  EventDescription		varchar(8000) 	NOT NULL,
+  DateTimePosted		datetime		NOT NULL DEFAULT (getdate()),
+  StartDate				datetime		NULL, 
+  EndDate				datetime		NULL,
+ 
+) 
+
+
+INSERT into myEvents (EventID,EventName,EventDescription,DateTimePosted,StartDate,EndDate) 
+VALUES (1, 'Borrow and win','Borrow 5 books during the event period and stand a chance to win a StarBucks gift card!','1-Nov-2021 03:09:20','15-Nov-2021','20-Nov-2021')
+
+INSERT into myEvents (EventID,EventName,EventDescription,DateTimePosted,StartDate,EndDate) 
+VALUES (2, 'Book Buffet','Give these books a second life. Grab a free book during this period','1-Nov-2022 03:09:20','15-Nov-2022','20-Nov-2022')
+
